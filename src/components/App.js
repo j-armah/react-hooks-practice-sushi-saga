55f56dc0671dde96f2f67f31451b27fb4144af7d
februar-y <jarmah241@gmail.com>
2021-01-16 11:04:40 -0500

    core deliv done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+
 function App() {
+  const [sushis, setSushis] = useState([])
+  const [plates, setPlates] = useState([])
+  const [money, setMoney] = useState(50)
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(data => {
+        //console.log(data)
+        setSushis(data)
+      })
+  }, [])
+
+  function handleSushiEaten(sushi) {
+    setPlates([...plates, sushi])
+    setMoney(money => money - sushi.price)
+  }
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushis} emptyPlateAdd={handleSushiEaten} money={money}/>
+      <Table plates={plates} money={money}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, emptyPlateAdd, money}) {
+  const [sushiStart, setSushiStart] = useState(0)
+  const [sushiEnd, setSushiEnd] = useState(4)
+  
+
+  const sushiArray = sushis.map(sushi => 
+    <Sushi key={sushi.id} sushi={sushi} emptyPlateAdd={emptyPlateAdd} money={money}/>
+  )
+
+  const handleMoreSushi = () => {
+    setSushiStart(sushiStart => sushiStart + 4)
+    setSushiEnd(sushiEnd => sushiEnd + 4)
+  }
+
+
+  
+  
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiArray.slice(sushiStart,sushiEnd)}
+      <MoreButton moreSushi={handleMoreSushi}/>
     </div>
   );
 }
